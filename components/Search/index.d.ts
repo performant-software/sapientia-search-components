d88743ce6356f0cae312f0ec38d2debd67658e66
camdendotlol <c@camdenmecklem.com>
2024-03-04 22:03:48 +0000

    Deploying to gh-pages from @ performant-software/sapientia-search-components@c2e97fadc15109d3cb92497efb6d385d77061c3b 🚀

--- components/Search/index.d.ts
+++ components/Search/index.d.ts
@@ -1,10 +1,11 @@
-/// <reference types="react" />
+import { ReactElement } from 'react';
 interface SearchProps {
     locale: 'en' | 'fr';
     onHitClick?: (arg: any) => void;
     hitWrapperComponent?: React.FC;
     project: 'bischoff' | 'rumpf' | 'supplique';
     getHitWrapperProps?: (...args: any) => any;
+    children?: ReactElement<any, string>;
 }
 declare const Search: React.FC<SearchProps>;
 export default Search;
